e28d5203f008bb74ec6689ba2a90fee5646f3cc6
arjunsoftland2025 <rarjunkrishnasoftland@gmail.com>
2025-03-20 17:37:31 +0530

    with setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='attendence',
+    version='0.0.1',
+    description='attendence app',
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+    zip_safe=False
+)
